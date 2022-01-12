218aa613b197f33851903de86802dee29a08d457
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-12 18:35:39 +0900

    ADD: Lazy Loading 적용 #2 - @ManyToOne(fetch = FetchType.LAZY)와 @Transactional 이용한 Lazy Loading

--- src/main/java/org/zerock/board/entity/Reply.java
+++ src/main/java/org/zerock/board/entity/Reply.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @ToString(exclude = "board") // ToString에서 board 제외
+// 연관관계가 있는 변수의 경우 해당 객체의 ToString도 가져와야 하기에 제외 해주는 것 잊지말기
 public class Reply extends BaseEntity{
 
     @Id
--- src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -28,8 +29,10 @@ public class BoardRepositoryTests {
         });
     }
 
-    // 자동으로 조인이 되어 쿼리가 실행됨을 알 수 있다.
+    // 지연로딩으로 인한 'no Session' 에러를 해결하기 위해 필요한 어노테이션
+    @Transactional
     @Test
+    // 자동으로 조인이 되어 쿼리가 실행됨을 알 수 있다.
     public void testRead1(){
         Optional<Board> result = boardRepository.findById(100L);
         Board board = result.orElse(null);
